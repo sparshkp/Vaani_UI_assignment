1f63f9d773de06fa2acbb64e7bc28c2923fff43b
Sparsh Kapoor <kapsparsh4@gmail.com>
2023-12-12 13:56:26 +0530

    new changes

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:sample/cards/card1.dart';
 import 'package:sample/cards/card2.dart';
@@ -25,10 +23,90 @@ class HomePage extends StatelessWidget {
               child: PageView(
                 scrollDirection: Axis.vertical,
                 controller: _controller,
-                children: const [
-                  Card1(),
-                  Card2(),
-                  Card3(),
+                children:  [
+                  Stack(
+                    children: [
+                      Padding(
+                      padding: const EdgeInsets.only(right: 35,left: 35,top: 70,bottom: 15),
+                      child: Container(
+                      height: 1000,
+                      width: 400,
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple[100],
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 35,left: 35,top: 55,bottom: 20),
+                      child: Container(
+                      height: 800,
+                      width: 400,
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple[200],
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 35,left: 35,top: 35,bottom: 30),
+                      child: Container(
+                        height: 900,
+                        width: 400,
+                        decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.deepPurple[300],
+                        ),
+                      ),
+                    ),
+                    Card1()
+                    ],
+                  ),
+                  // card2 
+                  Stack(
+                    children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 35,left: 35,top: 35,bottom: 20),
+                      child: Container(
+                      height: 800,
+                      width: 400,
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple[100],
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 35,left: 35,top: 35,bottom: 30),
+                      child: Container(
+                        height: 900,
+                        width: 400,
+                        decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.deepPurple[300],
+                        ),
+                      ),
+                    ),
+                    Card2()
+                    ],
+                  ),
+                  Stack(
+                    children: [
+
+                    Padding(
+                      padding: const EdgeInsets.only(right: 40,left: 40,top: 40,bottom: 30),
+                      child: Container(
+                        height: 900,
+                        width: 400,
+                        decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.deepPurple[300],
+                        ),
+                      ),
+                    ),
+                    Card3()
+                    ],
+                  ),
                   Card4(),
                 ],
               ),
@@ -60,3 +138,13 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
